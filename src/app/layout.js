7728c38726fcbeff2456b1b5cb12b79fdb00b3a5
noahbachmann <noah.bachmann@kauz.ch>
2025-06-26 11:18:16 +0200

    feat: use font family Inter

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
+import { Inter } from 'next/font/google'
+
 import './globals.css'
 import { Navigation, Footer } from './components'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
 	title: 'Portfolio',
@@ -8,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={ inter.className }>
 			<body className="flex flex-col min-h-screen">
 				<Navigation />
 
