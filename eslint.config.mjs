4fdfad0708fddd040d06047f1c11cdb690bebad6
noahbachmann <noah.bachmann@kauz.ch>
2025-06-26 09:25:52 +0200

    feat: adjust eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,32 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import stylistic from '@stylistic/eslint-plugin'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+	...compat.extends("next/core-web-vitals"),
+	{
+		files: [
+			"**/*.{js,ts,jsx,tsx}"
+		],
+		plugins: {
+			'@style': stylistic,
+		},
+		rules: {
+			'prefer-const': 'warn',
+			'no-unused-vars': 'warn',
+
+			'@style/no-trailing-spaces': 'warn',
+
+		}
+	}
+];
 
 export default eslintConfig;
