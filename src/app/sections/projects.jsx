7018864c32d7364cd2cb5f8f02e1a2f890cda47b
noahbachmann <noah.bachmann@kauz.ch>
2025-06-30 10:53:59 +0200

    feat: create scrollbar logic

--- src/app/sections/projects.jsx
+++ src/app/sections/projects.jsx
@@ -1,11 +1,11 @@
 import { ProjectCard } from '../components'
 import data from '@/data/projects.json'
 
-export default function Projects() {
+export default function Projects({ className = '' }) {
 	return(
-		<div id="projects" className="w-full min-h-full ps-20">
+		<div id="projects" className={ `w-full min-h-full ${className}` }>
 			<h2>Projects</h2>
-			<div className="relative flex gap-16 overflow-x-auto overscroll-contain">
+			<div className="relative flex gap-16 overflow-x-auto snap-x snap-mandatory *:snap-center no-scrollbar px-40">
 				{ data.map((project, index) => (
 					<ProjectCard
 						key={ index }
