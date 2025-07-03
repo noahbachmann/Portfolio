f2512f7c51194444ea7e6f036cb3a39b7eefd399
noahbachmann <noah.bachmann@kauz.ch>
2025-07-03 12:57:42 +0200

    fix: center header

--- src/app/sections/header.jsx
+++ src/app/sections/header.jsx
@@ -10,7 +10,7 @@ export default function Header({ className = '' }) {
 				<MatrixCanvas />
 			</div>
 
-			<div className={ `flex flex-col justify-center z-10 relative ${className}` }>
+			<div className={ `h-full flex flex-col justify-center z-10 relative ${className}` }>
 				<h1>My Portfolio</h1>
 				<hr className="w-128 mb-16" />
 				<p className="max-w-500 text-lead">I have a huge passion for computers, programming, logical thinking and everything around it!</p>
