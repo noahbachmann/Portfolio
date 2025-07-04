91986776fda1384950c01ba32890b96dcf5c8b93
noahbachmann <noah.bachmann@kauz.ch>
2025-07-04 10:40:52 +0200

    feat: create scroller animation

--- src/app/sections/projects.jsx
+++ src/app/sections/projects.jsx
@@ -1,21 +1,40 @@
+'use client'
+import { motion, useTransform, useScroll } from 'motion/react'
+import { useState, useRef, useEffect } from 'react'
+
 import { ProjectCard } from '../components'
 import data from '@/data/projects.json'
 
 export default function Projects({ className = '' }) {
+	const SECTION_REF = useRef(null)
+	const SCROLL_REF = useRef(null)
+	const [scrollInfo, setScrollInfo] = useState(0)
+
+	useEffect(() => {
+		setScrollInfo(SECTION_REF.current.offsetWidth - SCROLL_REF.current.scrollWidth - 32)
+	}, [])
+
+	const { scrollYProgress } = useScroll({
+		target: SECTION_REF,
+	})
+
+	const x = useTransform(scrollYProgress, [0, 1], [0, scrollInfo])
+
 	return(
 		<div id="projects" className={ `w-full min-h-full ${className}` }>
-			<div className="container container-lg p-40 flex flex-col">
-				<h2>My Projects</h2>
-				<p className="max-w-500 text-lead">In my short career, i've already been part of a lot of different projects. Some of them completely on my own,
-					some of them just part of a bigger team and one of them even as project-lead.<br/>
-					Keep scrolling to check out the most interesting of those projects...</p>
-			</div>
-			<div className="relative flex gap-16 overflow-x-auto snap-x snap-mandatory *:snap-center no-scrollbar px-40">
-				{ data.map((project, index) => (
-					<ProjectCard
-						key={ index }
-						project={ project } />
-				))}
+			<div ref={ SECTION_REF } className="h-[300vh] relative">
+				<div ref={ SCROLL_REF } className="h-screen px-32 sticky top-0 flex flex-col gap-24 justify-center overflow-hidden">
+					<motion.div style={{ x }} className=" flex gap-24">
+						{ data.map((project, index) => (
+							<ProjectCard
+								key={ index }
+								project={ project } />
+						))}
+					</motion.div>
+					<div className="container-sm w-full h-12 self-center rounded-xl overflow-hidden">
+						<motion.div style={{ scaleX:scrollYProgress, originX: 0, left:0 }} className="h-full bg-accent" />
+					</div>
+				</div>
 			</div>
 		</div>
 	)
