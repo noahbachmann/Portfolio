cfc195c45f4b5a0edab1848bf2dfa49d3893f6eb
noahbachmann <noah.bachmann@kauz.ch>
2025-07-08 10:52:11 +0200

    feat: change min-h on footer

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer({ className = '' }) {
 	return (
 		<footer className={`py-32 md:py-48 ${className}`}>
-			<div className="container container-lg w-full min-h-150 grid grid-cols-1 md:grid-cols-2">
+			<div className="container container-lg w-full min-h-100 grid grid-cols-1 md:grid-cols-2">
 				<div className="flex flex-col">
 					<p>Noah Bachmann</p>
 				</div>
