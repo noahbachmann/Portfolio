22ddf38a58d9584567e199def4785212c90af8c1
noahbachmann <noah.bachmann@kauz.ch>
2025-07-14 12:46:52 +0200

    feat: fix styling of contact section

--- src/app/components/contact-form.jsx
+++ src/app/components/contact-form.jsx
@@ -13,23 +13,23 @@ export default function ContactForm(){
 	return(
 		<form className="h-500 flex flex-col justify-evenly" onSubmit={ handleSubmit(OnSubmit) }>
 			<input
-				className="p-8 bg-white rounded h-[10%]"
+				className="h-[10%]"
 				type="email" placeholder="Your Name"
 				{ ...register('email', { required: true })} />
 
 			<input
-				className="p-8 bg-white rounded h-[10%]"
+				className="h-[10%]"
 				type="text"
 				placeholder="Subject"
 				{...register('subject')} />
 
 			<textarea
-				className="p-8 bg-white rounded h-[60%]"
+				className="h-[60%]"
 				type="text"
 				placeholder="Your message..."
 				{ ...register('message', { required: true })}/>
 
-			<button className="text-secondary bg-primary hover:text-black active:text-accent">Send</button>
+			<button className="button text-primary bg-secondary border-2 hover:text-white">Send</button>
 		</form>
 	)
 }
--- src/app/sections/contact.jsx
+++ src/app/sections/contact.jsx
@@ -7,19 +7,19 @@ function renderContent(tabNum) {
 		case 0:
 			return(
 				<>
-					<h3 className="text-primary">Contact</h3>
+					<h3 className="text-secondary">Contact</h3>
 				</>
 			)
 		case 1:
 			return (
 				<>
-					<h3 className="text-primary">My Socials</h3>
+					<h3 className="text-secondary">My Socials</h3>
 				</>
 			)
 		case 2:
 			return (
 				<>
-					<h3 className="text-primary">Send me something</h3>
+					<h3 className="text-secondary">Send me something</h3>
 					<ContactForm />
 				</>
 			)
@@ -31,16 +31,16 @@ function renderContent(tabNum) {
 export default function Contact({ className = '' }) {
 	const [info, setInfo] = useState(0)
 	return(
-		<div id="contact" className={ `container container-md size-full p-20 mb-24 grid grid-cols-1 md:grid-cols-2 gap-8 bg-secondary rounded-lg shadow ${className}` }>
-			<div className="relative bg-primary">
+		<div id="contact" className={ `container container-md size-full p-20 mb-24 grid grid-cols-1 md:grid-cols-2 bg-secondary rounded-lg shadow ${className}` }>
+			<div className="relative">
 				<h3>Noah Bachmann</h3>
-				<div className="h-full flex flex-col justify-evenly items-end absolute right-0">
-					<button className="bg-secondary" type="button" onClick={ () => setInfo(0) }>Contact</button>
-					<button className="bg-secondary" type="button" onClick={ () => setInfo(1) }>Socials</button>
-					<button className="bg-secondary" type="button" onClick={ () => setInfo(2) }>Mail</button>
+				<div className="h-full flex flex-col justify-evenly items-end absolute right-0 text-secondary">
+					<button className="md:-mr-1 px-12 py-8 cursor-pointer bg-primary rounded-l-l hover:scale-104 active:text-white active:scale-100 duration-200 ease-out" type="button" onClick={ () => setInfo(0) }>Contact</button>
+					<button className="md:-mr-1 px-12 py-8 cursor-pointer bg-primary rounded-l-l hover:scale-104 active:text-white active:scale-100 duration-200 ease-out" type="button" onClick={ () => setInfo(1) }>Socials</button>
+					<button className="md:-mr-1 px-12 py-8 cursor-pointer bg-primary rounded-l-l hover:scale-104 active:text-white active:scale-100 duration-200 ease-out" type="button" onClick={ () => setInfo(2) }>Mail</button>
 				</div>
 			</div>
-			<div className="min-h-500">
+			<div className="min-h-500 bg-primary p-24 rounded-lg">
 				{ renderContent(info) }
 			</div>
 		</div>
