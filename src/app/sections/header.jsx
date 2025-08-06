d6de14e1ec08b9313b987740ab0e006064c2697b
noahbachmann <noah.bachmann@kauz.ch>
2025-08-06 13:38:06 +0200

    fix: increase bg opacity for more clear text

--- src/app/sections/header.jsx
+++ src/app/sections/header.jsx
@@ -12,7 +12,7 @@ export default function Header({ className = '' }) {
 
 			<div className="container container-md min-h-[70vh] max-h-[80vh] h-full flex items-center">
 				<div className="pl-8 py-16 flex flex-col justify-center relative z-10">
-					<div className="size-full rounded-lg absolute inset-0 bg-primary opacity-60 backdrop-blur -z-10"/>
+					<div className="size-full rounded-lg absolute inset-0 bg-primary opacity-65 backdrop-blur -z-10"/>
 					<h1>My Portfolio</h1>
 					<hr className="w-128 h-3 mb-12 rounded-[2px]"/>
 					<p className="max-w-500 text-lead">I have a huge passion for computers, programming and everything around it. I hope you enjoy my website!</p>
