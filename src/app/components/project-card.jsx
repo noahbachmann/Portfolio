03359880eb9e06cc358346a84c3ca4e68d138a88
noahbachmann <noah.bachmann@kauz.ch>
2025-08-06 16:21:43 +0200

    feat: added project, resized images

--- src/app/components/project-card.jsx
+++ src/app/components/project-card.jsx
@@ -12,7 +12,7 @@ export default function ProjectCard({ project }) {
 
 	return(
 		<div className="flex flex-col bg-primary rounded-lg relative">
-			<div className="w-[95vw] max-w-400 md:min-w-500 h-250 md:h-300 flex overflow-hidden">
+			<div className="w-[95vw] max-w-400 md:min-w-500 md:max-w-500 h-250 md:h-300 flex overflow-hidden">
 				{ images.map((image, index) => (
 					<Image
 						key={ index }
@@ -28,7 +28,7 @@ export default function ProjectCard({ project }) {
 			<div className="flex flex-1 flex-col gap-20 px-24 py-20">
 				<div>
 					<h2>{ title }</h2>
-					<hr className="w-80 h-2 mb-6 rounded-[1px]"/>
+					<hr className="w-70 h-2 mb-6 rounded-[1px]"/>
 					<p>{ description }</p>
 				</div>
 
