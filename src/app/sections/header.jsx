57655dc6822dbda325fc24dc7a88fa145f2721bb
noahbachmann <noah.bachmann@kauz.ch>
2025-08-07 15:47:37 +0200

    feat: style changes

--- src/app/sections/header.jsx
+++ src/app/sections/header.jsx
@@ -10,7 +10,7 @@ export default function Header({ className = '' }) {
 				<MatrixCanvas />
 			</div>
 
-			<div className="container container-md min-h-[70vh] max-h-[80vh] h-full flex items-center">
+			<div className="container container-md min-h-[80vh] max-h-[95vh] md:min-h-[70vh] md:max-h-[80vh] h-full flex items-center">
 				<div className="pl-8 py-16 flex flex-col justify-center relative z-10">
 					<div className="size-full rounded-lg absolute inset-0 bg-primary opacity-65 backdrop-blur -z-10"/>
 					<h1>My Portfolio</h1>
